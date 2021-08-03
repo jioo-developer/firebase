44bdf1062ac1420e5952b4d78a065737f3f45f0c
jioo <rlawl3383@gmail.com>
2021-08-04 00:25:46 +0900

    zxa

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -9,8 +9,8 @@ function Edit(props) {
     let [textarea,setTextarea] =  useState("");
     let [file,setFile] = useState("");
     let [fileName,setFileName] = useState("")
-    let [deleteUrl,setDeleteUrl] =useState("")
-    let [checkDelete,setcheckDelete] = useState(false)
+    let [fileCheck,setFilecheck] = useState(false)
+    let [changeFile,setChangeFile] = useState(false)
     let user = props.user
     const history = useHistory();
     let locationEdit = props.reducer[0]
@@ -22,11 +22,22 @@ function Edit(props) {
     })
   },[])
 
-  useEffect(()=>{
-    setcheckDelete(true)
-  },[deleteUrl])
+     useEffect(()=>{
+        if(fileCheck){
+            let img = document.createElement("img");
+            img.src=`${file}`
+            img.classList.add("att")
+            document.querySelector("figure").append(img)
+        }
+        if(changeFile){
+            let img = document.createElement("img");
+            img.src=`${file}`
+            img.classList.add("att")
+            document.querySelector("figure").append(img)
+            }
+    },[file])
 
-    function onFileChange(e){
+     function onFileChange(e){
         const theFile = e.target.files[0];
         setFileName(theFile)
         const reader = new FileReader();
@@ -40,41 +51,16 @@ function Edit(props) {
             if(theFile){
                 reader.readAsDataURL(theFile)
             }
+
+        let findImg =document.querySelector(".att");
+        if(findImg !== null){
+            findImg.remove();
+            setChangeFile(true)
+        } else {
+            setFilecheck(true)
+        }
     }
 
-    async function post(e){
-        e.preventDefault();
-        let attchmentUrl =""; //이미지 추출경로 변수
-        if(file !== "" && file !== null){
-            const fileRef = storageService.ref().child(`${user.id}/${fileName.name}`)
-            const response = await fileRef.putString(file,"data_url");
-            attchmentUrl = await response.ref.getDownloadURL();
-        }
-
-        const content = {
-            title : title,
-            text: textarea,
-            url : attchmentUrl
-        }
-        await db.doc(`post/${locationEdit}`).update(content).then(()=>{
-            if(checkDelete){
-                storageService.refFromURL(deleteUrl).delete();
-            }
-            history.push(`/detail?id=${locationEdit}`)
-            window.alert("수정이 완료되었습니다.")
-        })
-
-    }
-
-    function clearPhoto(){
-        setFile(null)
-        let editImgs = document.querySelector(".edit_att");
-        if(editImgs !== null){
-            setDeleteUrl(posts.url)
-            editImgs.remove();
-        }
-        document.querySelector(".file-form").value=null;
-    }
 
     function enterEvent(){
         let height = document.querySelector(".text");
@@ -89,19 +75,43 @@ function Edit(props) {
         //미완
     }
 
+    async function clearPhoto(){
+        setFilecheck(true)
+        setChangeFile(false)
+        document.querySelector(".att").remove();
+    }
+
+    async function post(e){
+        e.preventDefault();
+        let attchmentUrl = "";
+        if(file !== ""){
+            const fileRef = storageService.ref().child(`${user.id}/${fileName.name}`)
+            const response = await fileRef.putString(file,"data_url");
+            attchmentUrl = await response.ref.getDownloadURL();
+        }
+
+        const content = {
+            title : title,
+            text : textarea,
+            url : attchmentUrl
+        }
+
+        await db.doc(`post/${locationEdit}`).update(content).then(()=>{
+            history.push(`/detail?id=${locationEdit}`)
+            window.alert("수정이 완료되었습니다.")
+        })
+    }
+
     return (
         <div className="upload">
             <form onSubmit={post}>
-                <input type="text" className="form-control titlearea" id="title" placeholder={posts.title === "" ? "제목을 입력해주세요" : posts.title}  maxLength={120} onChange={e=>setTitle(e.target.value)}/>
+                <input type="text" className="form-control titlearea" id="title" placeholder={posts.title === "" ? "제목을 입력해주세요" : posts.title} maxLength={120} onChange={e=>setTitle(e.target.value)}/>
                 <div className="textarea">
                     <textarea className="text" placeholder={posts.text === "" ? "당신의 이야기를 입력해주세요" : posts.text} onKeyUp={enterEvent} onChange={e=>setTextarea(e.target.value)}></textarea>
                     <figure>
-                    {
-                        posts.url !== "" ? <img src={posts.url} className="att edit_att" alt=""/> : ""
-                    }
-                    {
-                        file && <img src={file} className="att" alt=""/>
-                    }
+                        {
+                            posts.url === "" ? "" : <img src={posts.url} className="att" alt=""/>
+                        }
                     </figure>
                 </div>
                 <input type="file" accept="image/*" className="file-form" id="image" onChange={onFileChange}/>
